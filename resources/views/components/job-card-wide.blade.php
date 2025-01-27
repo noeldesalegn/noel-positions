24efc683d2e0ddfde875a73c4fc398df0a9b34aa
noeldesalegn <desalegnbekele2121@gmail.com>
2025-01-27 05:24:28 -0800

    fully tested added featured jobs , job seeder ,migration and more ...

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,21 +1,23 @@
+@props(['job'])
 <x-panel class="flex gap-x-6">
         <div>
             <x-employer-logo />
         </div>
-        
+
         <div class="flex-1 flex flex-col ">
             <a class="self-start text-sm text-gray-400">
-                Laracasts
+                {{ $job->employer->name }}
             </a>
-        
-                <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300 ">Senior Software Engineer</h3>
-                <p class="text-sm text-gray-400 mt-auto ">Full Time - From $60,000</p>
-    
+
+                <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300 ">{{ $job->title }} </h3>
+                <p class="text-sm text-gray-400 mt-auto ">{{ $job->salary }}</p>
+
         </div>
-        
-        <div"> 
-            <x-tag  size="small">Tag</x-tag>
-            <x-tag  size="small">Tag</x-tag>
-            <x-tag  size="small">Tag</x-tag>
+
+        <div>
+            @foreach($job->tags as $tag)
+                <x-tag :$tag />
+            @endforeach
+
         </div>
 </x-panel>
